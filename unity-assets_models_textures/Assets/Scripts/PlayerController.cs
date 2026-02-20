0d617d89b3b84db49b75eda7096577473ea76a69
C4lice <laura.aupetit002@gmail.com>
2026-02-20 18:00:28 +0100

    Added PlayerController script to manage player movement and jumping

---
+++ unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 6f;
+    public float jumpForce = 7f;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        float moveX = Input.GetAxis("Horizontal"); // A / D
+        float moveZ = Input.GetAxis("Vertical");   // W / S
+
+        Vector3 movement = new Vector3(moveX, 0f, moveZ);
+        Vector3 velocity = movement * moveSpeed;
+
+        Vector3 currentVelocity = rb.velocity;
+        rb.velocity = new Vector3(velocity.x, currentVelocity.y, velocity.z);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            isGrounded = true;
+        }
+    }
+}
